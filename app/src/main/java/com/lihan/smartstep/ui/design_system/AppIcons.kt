1433b5cec85525e23e2f5cc27998faa3e50ebac4
LiHanChen <lihan.lh.chen@ptc-nec.com.tw>
2026-02-04 11:27:17 +0800

    Initial Project

---
+++ app/src/main/java/com/lihan/smartstep/ui/design_system/AppIcons.kt
@@ -0,0 +1,53 @@
+package com.lihan.smartstep.ui.design_system
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.lihan.smartstep.R
+
+
+val Refresh: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.refresh)
+
+val ArrowsDown: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.arrows_down)
+
+val ArrowsUp: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.arrows_up)
+
+val ArrowsRight: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.arrows_right)
+
+val CheckMark: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.check_mark)
+
+val Close: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.close)
+
+val Menu: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.menu)
+
+val PenEdit: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.pen_edit)
+
+val PowerTurnOn: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.power_turn_on)
+
+val Sneaker: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.sneaker)
+
+val Road: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(R.drawable.road)
+
+
---
+++ app/src/main/java/com/lihan/smartstep/ui/theme/Type.kt
@@ -0,0 +1,78 @@
+package com.lihan.smartstep.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.lihan.smartstep.R
+
+val Inter = FontFamily(
+    fonts = listOf<Font>(
+        Font(R.font.inter , FontWeight.Normal),
+        Font(R.font.inter_medium , FontWeight.Medium),
+        Font(R.font.inter_bold , FontWeight.Bold),
+        Font(R.font.inter_semibold , FontWeight.SemiBold),
+    )
+)
+
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp,
+        lineHeight = 24.sp
+    )
+)
+
+val Typography.titleAccent: TextStyle
+    get() = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 64.sp,
+        lineHeight = 70.sp
+    )
+
+val Typography.bodyLargeRegular: TextStyle
+    get() = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp
+    )
+val Typography.bodyLargeMedium: TextStyle
+    get() = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp
+    )
+val Typography.bodyMediumRegular: TextStyle
+    get() = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 18.sp
+    )
+val Typography.bodyMediumMedium: TextStyle
+    get() = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 18.sp
+    )
+val Typography.bodySmallRegular: TextStyle
+    get() = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp
+    )
